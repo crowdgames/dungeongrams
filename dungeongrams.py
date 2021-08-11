012457264e44da3738c2989a6c56ba9b09c970c6
Seth Cooper <seth.cooper@gmail.com>
2021-08-11 14:27:25 -0400

    Add single-button control.

--- dungeongrams.py
+++ dungeongrams.py
@@ -520,12 +520,39 @@ def dosolve(level, state, thorough, slow):
 
 
 def play(levelfile, is_file, partial):
+    # https://stackoverflow.com/questions/510357/how-to-read-a-single-character-from-the-user
+    def _find_getch():
+        try:
+            import termios
+        except ImportError:
+            # Non-POSIX. Return msvcrt's (Windows') getch.
+            import msvcrt
+            return msvcrt.getch
+
+        # POSIX system. Create and return a getch that manipulates the tty.
+        import sys, tty
+        def _getch():
+            fd = sys.stdin.fileno()
+            old_settings = termios.tcgetattr(fd)
+            try:
+                tty.setraw(fd)
+                ch = sys.stdin.read(1)
+            finally:
+                termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
+            return ch
+
+        return _getch
+
+    getch = _find_getch()
+
     g = Game()
     g.loadself(levelfile, is_file, partial)
 
     while True:
         g.displayself()
-        action = input()
+        action = getch()
+        if action == 'q' or ord(action) == 3:
+            break
         g.stepself(action)
 
 
