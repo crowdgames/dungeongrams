2c3cb4877e7899e8ecae08c1f6ceb79675f7297b
bi3mer <colan@comcast.net>
2023-07-23 16:23:06 -0400

    some updates

--- difficulty.py
+++ difficulty.py
@@ -45,8 +45,22 @@ def proximity_to_enemies(level: List[str], path: List[Tuple[int, int]]) -> float
 
             if level[_y][_x] == '#':
                 in_proximity += 1/manhattan_distance(x, y, _x, _y)
-            # elif level[_y][_x] == '^':
-            #     in_proximity += 0.1*manhattan_distance(x, y, _x, _y)
+
+    return in_proximity / len(path)
+
+
+def proximity_to_food(level: List[str], path: List[Tuple[int, int]]) -> float:
+    in_proximity = 0
+    for (y, x) in path:
+        for (x_mod, y_mod) in ENEMY_RADIUS:
+            _x = x + x_mod
+            _y = y + y_mod
+
+            if _x < 0 or _y < 0 or _x >= len(level[0]) or _y >= len(level):
+                continue
+
+            if level[_y][_x] == '&':
+                in_proximity += 1/manhattan_distance(x, y, _x, _y)
 
     return in_proximity / len(path)
 
@@ -54,17 +68,17 @@ def density(level: List[str]) -> float:
     num_tiles = len(level) * len(level[0])
     return sum(sum(1 for t in r if t in SOLIDS) for r in level) / num_tiles
 
+def food_density(level: List[str]) -> float:
+    num_tiles = len(level) * len(level[0])
+    return sum(sum(1 for t in r if t =='&') for r in level) / num_tiles
+
 def linearity(level: List[str]) -> float:
+    num_tiles = len(level) * len(level[0])
     count = 0
     for col in level:
-        if '^' in col :
-            count += 1/3
-        if '#' in col:
-            count += 1/3
-        if '*' in col:
-            count += 1/3
+        count += sum(1 for t in col if t == '^' or t =='#' or t=='*')
 
-    return count / len(level)
+    return count / num_tiles
 
 def percent_difference(a: float, b: float) -> float:
     return abs(a-b) / ((a+b)/2)
@@ -82,7 +96,9 @@ columns = [
     'stamina-percent-enemies',
     'stamina-percent-nothing',
     'density',
-    'leniency'
+    'leniency',
+    'food-density',
+    'proximity-to-food'
 ]
 
 readable_f = open('output.txt', 'w')
@@ -93,6 +109,7 @@ computation_f.write('\n')
 baseline_f = open('baseline_difficulty.csv', 'w')
 baseline_f.write('level,difficulty\n')
 
+# i = 0
 data_iterator = tqdm(data)
 for lvl_key in data_iterator:
     data_iterator.set_description(lvl_key)
@@ -122,15 +139,17 @@ for lvl_key in data_iterator:
 
     # Build out the difficulty vector
     V = [ 
-        (len(path_with_enemies) - len(path_no_enemies)) / len(path_with_enemies),  # path difference no enemies
-        (len(path_with_enemies) - len(path_no_nothing)) / len(path_with_enemies),  # path difference no enemies and switches
-        jaccard_similarity(path_with_enemies, path_no_enemies),                    # path similarity no enemies
-        jaccard_similarity(path_with_enemies, path_no_nothing),                    # path similarity no enemies and switches
+        len(path_with_enemies) - len(path_no_enemies),                # path difference no enemies
+        len(path_with_enemies) - len(path_no_nothing),                # path difference no enemies and switches
+        jaccard_similarity(path_with_enemies, path_no_enemies),       # path similarity no enemies
+        jaccard_similarity(path_with_enemies, path_no_nothing),       # path similarity no enemies and switches
         proximity_to_enemies(level, path_with_enemies),
-        percent_difference(stamina_with_enemies, stamina_no_enemies),              # Percent difference of stamina at end no enemies
-        percent_difference(stamina_with_enemies, stamina_no_nothing),              # Percent difference of stamina at end no enemies and switches
-        density(level),                                                            # Density of the level
-        linearity(level),                                                          # linearity of the level
+        percent_difference(stamina_with_enemies, stamina_no_enemies), # Percent difference of stamina at end no enemies
+        percent_difference(stamina_with_enemies, stamina_no_nothing), # Percent difference of stamina at end no enemies and switches
+        density(level),                                               # Density of the level
+        linearity(level),                                             # linearity of the level
+        food_density(level),                                          # Density of food in the level
+        proximity_to_food(level, path_with_enemies),
     ]
 
     # convert to strings for convenience when writing
